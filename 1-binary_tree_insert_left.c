f98f3144d00d4798944195764866372275c205a2
jude iwuji <judeiwuji@gmail.com>
2022-11-14 13:52:58 +0100

    Add: Task 2. Insert right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -
+ * binary_tree_insert_left - inserts a node as the left-child of another node
  *
  * @parent: a pointer to the node to insert the left-child in
  * @value: the value to store in the new node
@@ -12,7 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL || node == NULL)
+	{
+		free(node);
 		return (NULL);
+	}
 
 	node->left = NULL;
 	node->right = NULL;
