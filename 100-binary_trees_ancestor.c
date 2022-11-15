360324c302632e632b5440d15eec01c1ae66ff1f
jude iwuji <judeiwuji@gmail.com>
2022-11-15 23:05:23 +0100

    update

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,27 +1,49 @@
 #include "binary_trees.h"
 
 /**
+ * tree_root - returns the root of a given node
+ *
+ * @node: The node to get the root
+ * Return: binary_tree_t*
+ */
+binary_tree_t *tree_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return ((binary_tree_t *)(node));
+	return (tree_root(node->parent));
+}
+
+/**
  * trees_ancestor - finds the lowest common ancestor of two nodes
  *
+ * @node: a pointer to the current node
  * @first: a pointer to the first node
  * @second: second is a pointer to the second node
  * Return: (binary_tree_t*)
  */
-binary_tree_t *trees_ancestor(const binary_tree_t *first,
-							  const binary_tree_t *second)
+binary_tree_t *lowest_common_ancestor(binary_tree_t *node,
+									  const binary_tree_t *first,
+									  const binary_tree_t *second)
 {
-	binary_tree_t *node;
+	binary_tree_t *left_tree;
+	binary_tree_t *right_tree;
 
-	if (first)
-	{
-		node = first->parent;
-		while (node)
-		{
-			if (node == second->parent || node == second)
-				return (node);
-			node = node->parent;
-		}
-	}
+	if (node == NULL)
+		return (NULL);
+	else if (node == first || node == second)
+		return (node);
+
+	left_tree = lowest_common_ancestor(node->left, first, second);
+	right_tree = lowest_common_ancestor(node->right, first, second);
+
+	if (left_tree != NULL && right_tree != NULL)
+		return (node);
+	else if (left_tree != NULL)
+		return (left_tree);
+	else
+		return (right_tree);
 	return (NULL);
 }
 
@@ -35,13 +57,11 @@ binary_tree_t *trees_ancestor(const binary_tree_t *first,
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second)
 {
-	binary_tree_t *node;
+	const binary_tree_t *tree;
 
 	if (first == NULL && second == NULL)
 		return (NULL);
 
-	node = trees_ancestor(first, second);
-	if (node)
-		return (node);
-	return (trees_ancestor(second, first));
+	tree = first != NULL ? first : second;
+	return (lowest_common_ancestor(tree_root(tree), first, second));
 }
