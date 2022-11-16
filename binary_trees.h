37e54c32e4c4d027e94745ec8a270757aaae6dda
jude iwuji <judeiwuji@gmail.com>
2022-11-16 07:03:10 +0100

    Add: Task 20. Level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -48,4 +48,14 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+/**
+ * struct binary_queue_S - Binary Tree Queue
+ * @next: the next node
+ */
+typedef struct binary_queue_s
+{
+	struct binary_tree_s *data;
+	struct binary_queue_s *next;
+} binary_queue_t;
 #endif /*  _BINARY_TREES_H_ */
